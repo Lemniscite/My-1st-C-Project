2562a2bc23c16777fce2e587b5926ef9e2690347
Lemniscite <60133566+Lemniscite@users.noreply.github.com>
2020-01-21 15:41:56 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,17 @@
+/*Averageof5nos-Hiro*/
+#include <stdio.h>
+
+void main()
+
+       { int no1,no2,no3,no4,no5;
+int avg,sum=0;
+printf("Kindly input 5 no's");
+scanf("%d %d %d %d %d",&no1,&no2,&no3,&no4,&no5);
+sum=no1+no2+no3+no4+no5;
+avg=sum/5;
+
+printf("sum is ");
+printf("%d",sum);
+printf("\nAverage is ");
+printf("%d",avg);
+} 	
